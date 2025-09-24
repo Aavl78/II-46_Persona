d229d221af47f6071073024e683e5ed3f5570168
User <User@DESKTOP-G73O90I>
2025-09-23 19:54:55 -0600

    Agregar archivos de proyecto.

---
+++ Default.aspx.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Web.ModelBinding
+
+Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Dim p1 As New Persona()
+        Dim p2 As New Persona(25)
+        Dim p3 As New Persona("juan")
+    End Sub
+End Class
---
+++ Models/Persona.vb
@@ -0,0 +1,43 @@
+﻿Public Class Persona
+    Private _nombre As String
+    Private _Apellido As String
+    Private _Edad As Integer
+
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property Apellido As String
+        Get
+            Return _Apellido
+        End Get
+        Set(value As String)
+            _Apellido = value
+        End Set
+    End Property
+
+    Public Property Edad As Integer
+        Get
+            Return _Edad
+        End Get
+        Set(value As Integer)
+            _Edad = value
+        End Set
+    End Property
+
+    Public Sub New(nombre As String)
+        Me.Nombre = "No hay nombre"
+    End Sub
+
+    Public Sub New(nombre As String, apellido As String, edad As Integer)
+        Me.Nombre = nombre
+        Me.Apellido = apellido
+        Me.Edad = edad
+    End Sub
+
+End Class
